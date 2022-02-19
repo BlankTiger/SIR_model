5a17e58876cb2c31b43ffe5d5b6c19e4021de722
Maciej Urban <macielurban@gmail.com>
2022-02-19 15:32:04 +0100

    Multiwave finished
    Multiwave functionality added to sir with vaccinations

--- utils/models.py
+++ utils/models.py
@@ -27,9 +27,7 @@ def SIR(t, y, beta, gamma, a, R_prev):
     return dydt
 
 
-def SIR_with_vaccination(
-    t, y, beta, gamma, vac_rate, eff, t_1, t_2, with_multiwave, a, t_3
-):
+def SIR_with_vaccination(t, y, beta, gamma, eff, vac_rate, a, R_prev):
     """Function modeling a SIR model with vaccination.
 
     Args:
@@ -49,37 +47,48 @@ def SIR_with_vaccination(
         dydt: vector of the derivatives of the variables
     """
     S, I, R = y
-    hev = utils.mathematics.heaviside_analytical
-    dydt = []
-    if with_multiwave:
-        if S - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0 and t < t_2:
-            dydt = [-S, beta * S * I - gamma * I, S + gamma * I]
-        elif S - beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0:
-            dydt = [
-                -beta * S * I + hev(t, t_3) * a * R,
-                beta * S * I - gamma * I,
-                gamma * I - hev(t, t_3) * a * R,
-            ]
-        else:
-            dydt = [
-                -beta * S * I
-                + hev(t, t_3) * a * R
-                - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
-                beta * S * I - gamma * I,
-                gamma * I
-                - hev(t, t_3) * a * R
-                + vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
-            ]
-        return dydt
+    diff_m = a * R_prev
+    diff_v = eff * vac_rate
+    if S - beta * S * I - diff_v < 0:
+        diff_v = 0
+    if R - diff_m < 0:
+        diff_m = 0
 
-    elif S - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0 and t < t_2:
-        dydt = [-S, beta * S * I - gamma * I, S + gamma * I]
-    elif S - beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0:
-        dydt = [-beta * S * I, beta * S * I - gamma * I, gamma * I]
-    else:
-        dydt = [
-            -beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
-            beta * S * I - gamma * I,
-            gamma * I + vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
-        ]
+    _S = -beta * S * I
+    _R = gamma * I
+    _I = beta * S * I - _R
+
+    dydt = [_S + diff_m - diff_v, _I, _R - diff_m + diff_v]
     return dydt
+    # if with_multiwave:
+    #     if S - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0 and t < t_2:
+    #         dydt = [-S, beta * S * I - gamma * I, S + gamma * I]
+    #     elif S - beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0:
+    #         dydt = [
+    #             -beta * S * I + hev(t, t_3) * a * R,
+    #             beta * S * I - gamma * I,
+    #             gamma * I - hev(t, t_3) * a * R,
+    #         ]
+    #     else:
+    #         dydt = [
+    #             -beta * S * I
+    #             + hev(t, t_3) * a * R
+    #             - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
+    #             beta * S * I - gamma * I,
+    #             gamma * I
+    #             - hev(t, t_3) * a * R
+    #             + vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
+    #         ]
+    #     return dydt
+
+    # elif S - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0 and t < t_2:
+    #     dydt = [-S, beta * S * I - gamma * I, S + gamma * I]
+    # elif S - beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)) <= 0:
+    #     dydt = [-beta * S * I, beta * S * I - gamma * I, gamma * I]
+    # else:
+    #     dydt = [
+    #         -beta * S * I - vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
+    #         beta * S * I - gamma * I,
+    #         gamma * I + vac_rate * eff * hev(t, t_1) * (1 - hev(t, t_2)),
+    #     ]
+    # return dydt
