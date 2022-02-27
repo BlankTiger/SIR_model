9d7f62afe23b12c45360283334d38824d9e0c98d
Maciej Urban <macielurban@gmail.com>
2022-02-27 18:27:20 +0100

    removed unnecessary code from math module

--- utils/mathematics.py
+++ utils/mathematics.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.integrate import solve_ivp
 import utils
 
 
@@ -94,78 +93,6 @@ def solve_SIR_with_vaccination(time_range, y0, t_1, t_2, with_multiwave, t_3, **
 
     sol = solution(np.array(time_points), np.array([S, I, R]))
     return sol
-
-
-# def solve_SIR_with_vaccination(
-#     t_range,
-#     y0,
-#     beta,
-#     gamma,
-#     vac_rate,
-#     eff,
-#     t_start,
-#     t_end,
-#     with_multiwave,
-#     a,
-#     t_3,
-#     t_values,
-# ):
-#     """Solves the SIR model with vaccinations using the
-#     scipy.integrate.solve_ivp function.
-
-#     Args:
-#         t_range (tuple): The time range of the simulation.
-#         y0 (tuple): The initial conditions.
-#         beta (float): The infection rate.
-#         gamma (float): The recovery rate.
-#         vac_rate (int): The vaccination rate.
-#         eff (float): The efficacy of the vaccination.
-#         t_start (int): The start time of the vaccination.
-#         t_end (int): The end time of the vaccination.
-#         t_values (list): The time values at which to calculate the solution.
-#         with_multiwave (bool): indicates if recovered people lose immunity
-#         a (float): proportion of recovered people who lose immunity
-#         t_3 (int): time of the start of immunity loss
-
-#     Returns:
-#         tuple: The solution of the SIR model with vaccinations.
-#     """
-#     SIR_with_vaccination = utils.models.SIR_with_vaccination
-#     return solve_ivp(
-#         SIR_with_vaccination,
-#         t_range,
-#         y0,
-#         t_eval=t_values,
-#         method="DOP853",
-#         args=(beta, gamma, vac_rate, eff, t_start, t_end, with_multiwave, a, t_3),
-#     )
-
-
-# def solve_SIR(t_range, y0, beta, gamma, with_multiwave, a, t_3, R_prev, t_values):
-#     """Solves the SIR model using the scipy.integrate.solve_ivp function.
-
-#     Args:
-#         t_range (tuple): The time range of the simulation.
-#         y0 (tuple): The initial conditions.
-#         beta (float): The infection rate.
-#         gamma (float): The recovery rate.
-#         t_values (list): The time values at which to calculate the solution.
-#         with_multiwave (bool): indicates if recovered people lose immunity
-#         a (float): proportion of recovered people who lose immunity
-#         t_3 (int): time of the start of immunity loss
-
-#     Returns:
-#         tuple: The solution of the SIR model.
-#     """
-#     SIR = utils.models.SIR
-#     return solve_ivp(
-#         SIR,
-#         t_range,
-#         y0,
-#         t_eval=t_values,
-#         method="DOP853",
-#         args=(beta, gamma, with_multiwave, a, t_3, R_prev),
-#     )
 
 
 def heaviside_analytical(t, tau):
