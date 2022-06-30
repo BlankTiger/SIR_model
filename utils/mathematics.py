2e89078af918ef40fd1509764a82a5e1c283d5d4
Maciej Urban <macielurban@gmail.com>
2022-06-30 16:59:36 +0200

    Added license file, fixed shown time, fixed last vaccination day shown

--- utils/mathematics.py
+++ utils/mathematics.py
@@ -23,7 +23,7 @@ def solve_SIR(time_range, y0, with_multiwave, t_3, **args):
 
         dt2 = dt / 2
         prev_y = np.array([S[t], I[t], R[t]])
-        if with_multiwave is True and t > t_3:
+        if with_multiwave is True and t >= t_3:
             args["R_prev"] = R[t - t_3]
 
             k1 = np.array(f(t, prev_y, **args))
@@ -58,42 +58,55 @@ def solve_SIR_with_vaccination(time_range, y0, t_1, t_2, with_multiwave, t_3, **
     I[0] = y0[1]
     R[0] = y0[2]
 
+    population = np.sum(y0)
+
     args["R_prev"] = 0
+    args["population"] = population
     vac_rate = args["vac_rate"]
     for t in range(len(time_points) - 1):
+        curr_t = time_points[t]
 
         dt2 = dt / 2
         prev_y = np.array([S[t], I[t], R[t]])
 
-        if not t_1 < t < t_2:
-            args["vac_rate"] = 0
-        else:
+        if t_1 <= curr_t <= t_2:
             args["vac_rate"] = vac_rate
+        else:
+            args["vac_rate"] = 0
 
-        if with_multiwave is True and t > t_3:
+        if with_multiwave and curr_t >= t_3:
             args["R_prev"] = R[t - t_3]
 
-            k1 = np.array(f(t, prev_y, **args))
-            k2 = np.array(f(t + dt2, prev_y + dt2 * k1, **args))
-            k3 = np.array(f(t + dt2, prev_y + dt2 * k2, **args))
-            k4 = np.array(f(t + dt, prev_y + dt * k3, **args))
-            new_y = np.maximum(prev_y + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4), 0)
-            S[t + 1] = new_y[0]
-            I[t + 1] = new_y[1]
-            R[t + 1] = new_y[2]
+            k1 = np.array(f(curr_t, prev_y, **args))
+            k2 = np.array(f(curr_t + dt2, prev_y + dt2 * k1, **args))
+            k3 = np.array(f(curr_t + dt2, prev_y + dt2 * k2, **args))
+            k4 = np.array(f(curr_t + dt, prev_y + dt * k3, **args))
+            S[t + 1], I[t + 1], R[t + 1] = np.maximum(
+                prev_y + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4), 0
+            )
+            if S[t + 1] >= population:
+                S[t + 1] = population
+            elif I[t + 1] >= population:
+                I[t + 1] = population
+            elif R[t + 1] >= population:
+                R[t + 1] = population
 
         else:
-            k1 = np.array(f(t, prev_y, **args))
-            k2 = np.array(f(t + dt2, prev_y + dt2 * k1, **args))
-            k3 = np.array(f(t + dt2, prev_y + dt2 * k2, **args))
-            k4 = np.array(f(t + dt, prev_y + dt * k3, **args))
-            new_y = np.maximum(prev_y + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4), 0)
-            S[t + 1] = new_y[0]
-            I[t + 1] = new_y[1]
-            R[t + 1] = new_y[2]
+            k1 = np.array(f(curr_t, prev_y, **args))
+            k2 = np.array(f(curr_t + dt2, prev_y + dt2 * k1, **args))
+            k3 = np.array(f(curr_t + dt2, prev_y + dt2 * k2, **args))
+            k4 = np.array(f(curr_t + dt, prev_y + dt * k3, **args))
+            S[t + 1], I[t + 1], R[t + 1] = np.maximum(
+                prev_y + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4), 0
+            )
+            if S[t + 1] >= population:
+                S[t + 1] = population
+            elif I[t + 1] >= population:
+                I[t + 1] = population
+            elif R[t + 1] >= population:
+                R[t + 1] = population
 
-    sol = solution(np.array(time_points), np.array([S, I, R]))
-    return sol
+    return solution(np.array(time_points), np.array([S, I, R]))
 
 
 def find_max_and_argmax(x, y):
--- utils/models.py
+++ utils/models.py
@@ -10,16 +10,16 @@ def SIR(t, y, beta, gamma, a, R_prev):
     return dydt
 
 
-def SIR_with_vaccination(t, y, beta, gamma, eff, vac_rate, a, R_prev):
+def SIR_with_vaccination(t, y, beta, gamma, eff, vac_rate, a, R_prev, population):
     S, I, R = y
     diff_m = a * R_prev
     diff_v = eff * vac_rate
-    if S - beta * S * I - diff_v < 0:
-        diff_v = S
 
-    _S = -beta * S * I
-    _R = gamma * I
-    _I = beta * S * I - _R
+    _S = -beta * S * I + diff_m - diff_v
+    _I = beta * S * I - gamma * I
+    _R = gamma * I - diff_m + diff_v
 
-    dydt = [_S + diff_m - diff_v, _I, _R - diff_m + diff_v]
-    return dydt
+    if R >= population:
+        _R = population - R
+
+    return [_S, _I, _R]
